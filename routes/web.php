cca9d844fe4fa958b35c3e20d2d650f611edb8f0
Sergey Polyakov <serejka.poliakov@yandex.ru>
2020-08-13 13:11:46 +0300

    Правки в конфиг, миграция и добавление админа

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,9 @@ use App\User;
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
-//Route::get('/partners', 'partnersController@index')->name('partners');
-//Route::get('/products', 'productsController@index')->name('products');
-//Route::get('/unloading', 'unloadingController@index')->name('unloading');
+Route::get('/partners', 'partnersController@index')->name('partners');
+Route::get('/products', 'productsController@index')->name('products');
+Route::get('/unloading', 'unloadingController@index')->name('unloading');
 
 Route::get('/user/{id}', function (\App\User $id) {
     return view('/user/user',['id' => $id]);
