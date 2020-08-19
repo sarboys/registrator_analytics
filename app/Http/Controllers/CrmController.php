00c9d6345815c6b0c194e53454048f608e8a352a
Sergey Polyakov <serejka.poliakov@yandex.ru>
2020-08-19 20:46:37 +0300

    Добавление 2х методов для работа со сделками

--- app/Http/Controllers/CrmController.php
+++ app/Http/Controllers/CrmController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class CrmController extends Controller
 {
     public function index() {
-        return view('/portal/crm');
+        $response = Http::post('https://portal.keydisk.ru/rest/896/'.env('APP_PORTAL_KEY').'/crm.timeline.comment.list', [
+            'order' => array(),
+            'filter' => array(
+                'ENTITY_ID' => 207841,
+                'ENTITY_TYPE' => 'Deal'
+            ),
+            'select' => array('ID','COMMENT','*','DATE_CREATE')
+        ]);
+
+        $response = Http::post('https://portal.keydisk.ru/rest/896/'.env('APP_PORTAL_KEY').'/crm.activity.list', [
+            'order' => array(),
+            'filter' => array(
+                'OWNER_ID' => 207841,
+                'OWNER_TYPE_ID' => 2
+            ),
+            'select' => array('*')
+        ]);
+//        return view('/portal/crm')->with('response',$res);
     }
 }
