ed153e4a9b455cfae6096417f4c9894f836ad801
Sergey Polyakov <serejka.poliakov@yandex.ru>
2020-08-19 15:38:35 +0300

    Исправлены функции для резалта

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -12,23 +12,7 @@ class PortalController extends Controller
     public $FailVoix;
     public $AllVoix;
     public $AllResult;
-
-    public function FailVoix ($phone) {
-        $response = Http::post('https://portal.keydisk.ru/rest/896/'.env('APP_PORTAL_KEY').'/voximplant.statistic.get', [
-            'order' => array(),
-            'filter' => array(
-                'PORTAL_NUMBER'=>$phone,
-                'CALL_TYPE' => 2,
-                'CALL_FAILED_CODE' => 304,
-                ">=CALL_START_DATE" => date('c',strtotime('2020-08-03T09:00')),
-                "<=CALL_START_DATE" => date('c',strtotime('2020-08-04T18:00'))
-            ),
-            'sort' => array()
-        ]);
-        $res = json_decode($response,true);
-        return $res['total'];
-    }
-    public function AllVoix($phone) {
+    public function AllVoix($phone,$dateFrom,$dateTo,$callDuration) {
         $response = Http::post('https://portal.keydisk.ru/rest/896/'.env('APP_PORTAL_KEY').'/voximplant.statistic.get', [
             'order' => array(),
             'filter' => array(
@@ -43,21 +27,40 @@ class PortalController extends Controller
         $res = json_decode($response,true);
         return $res['total'];
     }
-  public function index() {
-//        dd(date(DATE_ISO8601, strtotime('08/13/2020 12:00')));
-      $this->AllResult['allPhones'] = PortalPhone::all();
-      $this->AllResult['fail'] = $this->FailVoix('74951182890');
-      $this->AllResult['success'] = $this->AllVoix('74951182890');
-      $this->AllResult['all'] = round($this->FailVoix('74951182890') / $this->AllVoix('74951182890')  * 100,0);
+    public function FailVoix ($phone,$dateFrom,$dateTo) {
+        $response = Http::post('https://portal.keydisk.ru/rest/896/'.env('APP_PORTAL_KEY').'/voximplant.statistic.get', [
+            'order' => array(),
+            'filter' => array(
+                'PORTAL_NUMBER'=>$phone,
+                'CALL_TYPE' => 2,
+                'CALL_FAILED_CODE' => 304,
+                ">=CALL_START_DATE" => date('c',strtotime('2020-08-03T09:00')),
+                "<=CALL_START_DATE" => date('c',strtotime('2020-08-04T18:00'))
+            ),
+            'sort' => array()
+        ]);
+        $res = json_decode($response,true);
+        return $res['total'];
+    }
 
-      return view('portal')->with('response',$this->AllResult);
-  }
-    public function indexPost(Request $request) {
+
+    public function AllResult($phone,$dateFrom,$dateTo,$callDuration) {
         $this->AllResult['allPhones'] = PortalPhone::all();
-        $this->AllResult['fail'] = $this->FailVoix($request['param']);
-        $this->AllResult['success'] = $this->AllVoix($request['param']);
-        $this->AllResult['all'] = round($this->FailVoix($request['param']) / $this->AllVoix($request['param'])  * 100,0);
-        $this->AllResult['phone'] = $request['param'];
+
+
+        $this->AllResult['fail'] = $this->FailVoix($phone,$dateFrom,$dateTo);
+        $this->AllResult['success'] = $this->AllVoix($phone,$dateFrom,$dateTo,$callDuration);
+        $this->AllResult['all'] = round($this->FailVoix($phone,$dateFrom,$dateTo) / $this->AllVoix($phone,$dateFrom,$dateTo,$callDuration)  * 100,0);
         return view('portal')->with('response',$this->AllResult);
+    }
+
+
+
+
+    public function index() {
+        return $this->AllResult('74951182890','2020-01-01T09:00','2020-12-31T18:00','');
+    }
+    public function indexPost(Request $request) {
+//        return $this->AllResult($request['param']);
     }
 }
